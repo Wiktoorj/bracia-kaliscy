8ae4d012ae17df5d2210402c46bd60a3ff92115c
Wiktor Jackowiak <jackowiak.g.wiktor@gmail.com>
2021-11-24 20:03:35 +0100

    rozwoj strony

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-const time = 1500;
+const partnersSlideTime = 2000;
+
+
+
 const img = document.querySelector('.partners img')
 let partnerNumber = 0;
 
 const changePartnerImg = () => {
-img.src = `img/partnersSmall/partners${partnerNumber <= 5 ? partnerNumber++ : partnerNumber = 0}.png`
+img.src = `img/partnersSmall/partner${partnerNumber <= 5 ? partnerNumber++ : partnerNumber = 0}.png`
 }
 
-setInterval(changePartnerImg, time)
+setInterval(changePartnerImg, partnersSlideTime)
