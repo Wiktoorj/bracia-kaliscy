67d10b8d84fc04aff5ead2b468ffb64e1c94ba77
Wiktor Jackowiak <jackowiak.g.wiktor@gmail.com>
2022-01-20 23:22:59 +0100

    duzo zmian, powrot po przerwie

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+//HAMBURGERshow
+// const showPosition = () => {
+//     const burger = document.querySelector('.burger')
+//     const position = window.scrollY;
+//     const afterheader = document.querySelector('.target').offsetTop
+//     let flag = true;
+
+//     if (position > afterheader / 4) {
+//         burger.classList.remove('head')
+//         flag = false;
+//     } else {
+//         burger.classList.add('head')
+//         flag = true;
+//     }
+
+//     console.log(position, afterheader / 3, flag)
+// }
+// const afterheader = document.querySelector('.target').offsetTop
+
+// window.addEventListener('scroll', showPosition)
+
 //burger
 const burgerIcons = document.querySelectorAll('.burgerFas');
 const burgerNavi = document.querySelectorAll('nav ul li a');
@@ -6,6 +27,8 @@ const burger = function () {
     document.querySelector('nav').classList.toggle('off');
     for (ico of burgerIcons) ico.classList.toggle('off');
     document.querySelector('.burger').classList.toggle('off');
+    document.querySelector('.logoFirmyTop').classList.toggle('off');
+    document.querySelector('.nazwaFirmyTop').classList.toggle('off');
 }
 
 for (ico of burgerIcons) ico.addEventListener('click', burger);
@@ -35,13 +58,79 @@ const writeQuote = () => {
 let maszynaQuote = setInterval(writeQuote, 20)
 
 //reklama firm [slider]
-const partnersSlideTime = 2000;
-const divs = document.querySelectorAll('.dots div')
+const time = 2000;
+const divs = [...document.querySelectorAll('.dots div')];
 const img = document.querySelector('.partners img')
-let partnerNumber = 0;
 
-const changePartnerImg = () => {
-    img.src = `img/partnersSmall/partner${partnerNumber <= 5 ? partnerNumber++ : partnerNumber = 0}.png`
+const slide = [{
+        img: "img/partnersSmall/partner0.png"
+    },
+    {
+        img: "img/partnersSmall/partner1.png"
+    },
+    {
+        img: "img/partnersSmall/partner2.png"
+    },
+    {
+        img: "img/partnersSmall/partner3.png"
+    },
+    {
+        img: "img/partnersSmall/partner4.png"
+    },
+    {
+        img: "img/partnersSmall/partner5.png"
+    }
+]
+
+let slideIndex = 0;
+
+const changeDot = () => {
+    const dotIndex = divs.findIndex(div => div.classList.contains('active'))
+    divs[dotIndex].classList.remove('active');
+    divs[slideIndex].classList.add('active');
 }
 
-setInterval(changePartnerImg, partnersSlideTime);
+const changeSlide = () => {
+    slideIndex++;
+    if (slideIndex == slide.length) slideIndex = 0;
+    img.src = slide[slideIndex].img;
+    changeDot()
+}
+
+let indexInterval = setInterval(changeSlide, time)
+
+const dontRepeatUrself = function () {
+    if (slideIndex < 0) slideIndex = slide.length - 1
+    else if (slideIndex == slide.length) slideIndex = 0;
+    img.src = slide[slideIndex].img;
+    changeDot()
+    indexInterval = setInterval(changeSlide, time)
+}
+
+const changeManualy = function (e) {
+    if (e.keyCode === 37 || e.keyCode === 39) {
+        clearInterval(indexInterval);
+        e.keyCode === 37 ? slideIndex-- : slideIndex++
+        dontRepeatUrself()
+    }
+}
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+const changeManualyMobileLeft = function () {
+    clearInterval(indexInterval);
+    slideIndex--
+   dontRepeatUrself()
+}
+
+const changeManualyMobileRight = function () {
+    clearInterval(indexInterval);
+    slideIndex++
+    dontRepeatUrself()
+}
+
+
+window.addEventListener('keydown', changeManualy)
+left.addEventListener('click', changeManualyMobileLeft)
+right.addEventListener('click', changeManualyMobileRight)
